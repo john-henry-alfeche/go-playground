a056398ddbd7b6e24d242fbc3f5035eae44f80d4
alfeche-dev <alfeche.dev@gmail.com>
2025-04-28 21:32:20 +0800

    Day 2: Wrote Hello Go program and practiced printing fmt

---
+++ hello_go/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func main() {
+	fmt.Println("Hello, Go")
+	fmt.Println("I'm excited to learn GoLang!")
+	fmt.Println("My name is John Henry Alfeche.")
+	fmt.Println("I believe learning Go will be awesome!")
+	fmt.Println("Let's go!")
+
+}
